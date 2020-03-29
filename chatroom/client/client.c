c69b46220b832126f2cc9a77fef69ee30b490b51
suyelu <suyelu@haizeix.com>
2020-03-29 18:30:40 +0800

    chatroom_with_client

---
+++ chatroom/client/client.c
@@ -0,0 +1,35 @@
+/*************************************************************************
+	> File Name: client.c
+	> Author: suyelu
+	> Mail: suyelu@haizeix.com
+	> Created Time: 日  3/29 18:16:00 2020
+ ************************************************************************/
+
+#include "../common/chatroom.h"
+#include "../common/tcp_client.h"
+#include "../common/common.h"
+
+char *conf = "./client.conf";
+
+int main() {
+    int port, sockfd;
+    struct Msg msg;
+    char ip[20] = {0};
+    port = atoi(get_value(conf, "SERVER_PORT"));
+    strcpy(ip, get_value(conf, "SERVER_IP"));
+    printf("ip = %s , port = %d\n", ip, port);
+    
+    if ((sockfd = socket_connect(ip, port)) < 0) {
+        perror("socket_connect");
+        return 1;
+    }
+    strcpy(msg.from, get_value(conf, "MY_NAME"));
+    msg.flag = 2;
+    if (chat_send(msg, sockfd) < 0) {
+        return 2;
+    }
+    
+    return 0;
+}
+
+
